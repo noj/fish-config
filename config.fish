f85b752b1b57916e3f24a093812f4a58e2a22474
Jon Olsson <jon.olsson@gmail.com>
2024-12-06 18:41:12 +0100

    Pretty up a bit

--- config.fish
+++ config.fish
@@ -15,17 +15,27 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 bind --user -M insert \ck kill-line
 bind --user -M normal \ck kill-line
+
 bind --user -M insert \ca beginning-of-line
 bind --user -M normal \ca beginning-of-line
+
 bind --user -M insert \ce end-of-line
 bind --user -M normal \ce end-of-line
+
 bind --user -M insert \cp up-or-search
 bind --user -M normal \cp up-or-search
+
 bind --user -M insert \cn down-or-search
 bind --user -M normal \cn down-or-search
+
 bind --user -M insert \cr history-pager
 bind --user -M normal \cr history-pager
 
+# This prompt will look something like this:
+#
+# [I] jon@owl:~/s/g/n/monkey.odin (main)
+# >
+#
 function fish_prompt -d "Write out the prompt"
     printf '%s%s@%s%s:%s%s%s%s%s%s\n> ' (set_color bryellow) $USER (prompt_hostname) (set_color normal) \
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) \
